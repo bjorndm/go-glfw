7bcf8753b60c22da0e67e14248681be0580fa0d5
Björn DM <bjorn.de.meyer@no-spam.google.com>
2023-03-04 22:16:49 +0900

    Fix deps build hack also.

--- glfw/deps/dummy.go
+++ glfw/deps/dummy.go
@@ -6,7 +6,7 @@ package dummy
 
 import (
 	// Prevent go tooling from stripping out the c source files.
-	_ "github.com/bjorndm/go-glfwff/deps/glad"
-	_ "github.com/bjorndm/go-glfwff/deps/mingw"
-	_ "github.com/bjorndm/go-glfwff/deps/vs2008"
+	_ "github.com/bjorndm/go-glfwff/glfw/deps/glad"
+	_ "github.com/bjorndm/go-glfwff/glfw/deps/mingw"
+	_ "github.com/bjorndm/go-glfwff/glfw/deps/vs2008"
 )
